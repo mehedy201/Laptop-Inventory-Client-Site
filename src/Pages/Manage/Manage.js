fd4fc35b42d57b4787a92217b46600cc50429894
mehedy201 <mehedy643@gmail.com>
2022-05-09 19:23:15 +0600

    finish delete option in manage item

--- src/Pages/Manage/Manage.js
+++ src/Pages/Manage/Manage.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import useProducts from '../../Hooks/useProducts';
-import ManageProduct from './ManageProduct/ManageProduct';
+import './Manage.css'
 
 const Manage = () => {
     // Load Data using custom hooks
-    const [products] = useProducts();
+    const [products, setProducts] = useProducts();
+
+    // Handle delet items 
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure to Delete');
+        if(proceed){
+            const url = `http://localhost:4000/products/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const remain = products.filter(product => product._id !== id);
+                setProducts(remain);
+            })
+        }
+    }
 
     return (
         <div className='container mb-5'>
@@ -12,7 +29,17 @@ const Manage = () => {
             <h6>Total Product: {products.length}</h6>
             <div>
                 {
-                    products.map(product => <ManageProduct key={product._id} product={product}></ManageProduct>)
+                    products.map(product => <div>
+                         <div className='d-flex justify-content-between manage_product my-2 p-2 shadow-sm'>
+                            <div className='d-flex align-items-center'>
+                                <img src={product.img} alt="" />
+                                <h5 className='me-2'>{product.name}</h5>
+                                <p className='mx-2 my-0 p-0'>Price: ${product.price}</p>
+                                <p className='mx-2 my-0 p-0'>Quantity: {product.quantity}</p>
+                            </div>
+                            <button onClick={() => handleDelete(product._id)} className=' btn bg-danger text-white rounded-circle px-3 py-1 me-3'>X</button>
+                        </div>
+                    </div>)
                 }
             </div>
         </div>
